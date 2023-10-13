ff29316a58b94ebc9a1efe3291ee95690b6af84a
Josue072003 <josue.espinoza2@unmsm.edu.pe>
2023-10-12 23:22:17 -0500

    login terminado

--- Desarrollo/DS/Codigo/ENTORNO/src/app.py
+++ Desarrollo/DS/Codigo/ENTORNO/src/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, render_template, redirect, url_for, session
-from flask_login import LoginManager,login_user,logout_user
 from clases.consulta import ConexionSQLServer
 import uuid
 from datetime import datetime
@@ -82,11 +81,14 @@ def loginUsuario():
 			session['celular'] = datos_usuario[8]
 			session['admin'] = datos_usuario[9]
 			session['foto_perfil'] = datos_usuario[10]
+			session['logged'] = True
 			msg = 'Logeado con éxito'
-			return redirect(url_for('registroDenuncia')) # FALTA DEFINIR PANTALLA PRINCIPAL
+			if session['admin'] == 1:
+				return redirect(url_for('admin'))
+			elif session['admin'] == 0:
+				return redirect(url_for('registroDenuncia'))
 		else:
 			msg = 'Correo o contraseña incorrectos'
-	
 	return render_template('inicioSesion.html')
 
 @app.route('/logout/')
@@ -98,40 +100,46 @@ def cerrarSesion():
 def registroDenuncia():
 	msg = ''
 	verificado = False
-	'''
-	if request.method == 'POST':
-		
-		titulo = request.form['titulo']
-		descripcion = request.form['descripcion']
-		ID_usuario = session['ID_usuario']
-		fecha_creac = datetime.now()
-		fecha_creac = fecha_creac.strftime("%Y-%m-%d %H:%M:%S")
-		relevancia = 0
-		URL_imagen = request.form['URL_imagen'] # FALTA ACORDAR CUANDO SON VARIAS IMAGENES
-		while not verificado:
-			ID_publicacion = uuid.uuid4()
-			SQL = ConexionSQLServer('DESKTOP-0QQGSJL', 'DS-BBDD')
-			SQL.getUUIDPublicacion(ID_publicacion)
-			if SQL.encontrarUUIDPublicacion() == None:
-				array = (ID_publicacion, titulo, descripcion, ID_usuario, fecha_creac, relevancia)
-				SQL.getDatosPublicacion(array)
-				SQL.insertarDenuncia()
-				msg = 'Registro con éxito'
-				verificado = True
-		# FALTA INSERTAR IMAGENES EN LA TABLA
-	else:
-		msg = 'Método HTTP incorrecto'
-	'''
-	return render_template('registrarDenuncia.html')
+	try:
+		if session['logged'] == True and session['admin'] == 0:
+			'''
+			if request.method == 'POST':
+				
+				titulo = request.form['titulo']
+				descripcion = request.form['descripcion']
+				ID_usuario = session['ID_usuario']
+				fecha_creac = datetime.now()
+				fecha_creac = fecha_creac.strftime("%Y-%m-%d %H:%M:%S")
+				relevancia = 0
+				URL_imagen = request.form['URL_imagen'] # FALTA ACORDAR CUANDO SON VARIAS IMAGENES
+				while not verificado:
+					ID_publicacion = uuid.uuid4()
+					SQL = ConexionSQLServer('DESKTOP-0QQGSJL', 'DS-BBDD')
+					SQL.getUUIDPublicacion(ID_publicacion)
+					if SQL.encontrarUUIDPublicacion() == None:
+						array = (ID_publicacion, titulo, descripcion, ID_usuario, fecha_creac, relevancia)
+						SQL.getDatosPublicacion(array)
+						SQL.insertarDenuncia()
+						msg = 'Registro con éxito'
+						verificado = True
+				# FALTA INSERTAR IMAGENES EN LA TABLA
+			else:
+				msg = 'Método HTTP incorrecto'
+			'''
+			return render_template('registrarDenuncia.html')
+	except:
+		msg = 'Para acceder a esta página debes iniciar sesión'
+		return redirect(url_for('loginUsuario'))
 
 @app.route('/seguimiento-denuncia/', methods = ['GET', 'POST'])
 def seguimientoDenuncia():
 	msg = ''
-	ID_publicacion = request.form['ID_publicacion']
-	# ******************************************************************************************************
-	# Aquí se debe buscar la ID en la BBDD y si está se prosigue con la muestra de datos
-	# ******************************************************************************************************
 	return render_template('seguimientoDenuncia.html', msg) 
+
+@app.route('/admin/', methods = ['GET', 'POST'])
+def admin():
+	msg = ''
+	return render_template('admin.html', msg) 
 
 if __name__ == '__main__':
 	app.run(debug = True)
